20da01e67d3289a2cb0b98aa69139fb11f70d431
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2021-10-02 23:38:01 +0530

    JavaScript

---
+++ Javascript/calc.js
@@ -0,0 +1,28 @@
+function add(){
+    var a = document.getElementById('first').value;
+    var b = document.getElementById('second').value;
+    var out = Number(a)+Number(b)
+    document.getElementsByClassName('output')[0].innerText="Addition is "+out
+}
+
+
+function sub(){
+    var a = document.getElementById('first').value;
+    var b = document.getElementById('second').value;
+    var out = Number(a)-Number(b)
+    document.getElementsByClassName('output')[0].innerText="Subtraction is "+out
+}
+function mul(){
+    var a = document.getElementById('first').value;
+    var b = document.getElementById('second').value;
+    var out = Number(a) * Number(b)
+    document.getElementsByClassName('output')[0].innerText="Product is "+out
+
+}
+function div(){
+    var a = document.getElementById('first').value;
+    var b = document.getElementById('second').value;
+    var out = Number(a) / Number(b)
+    document.getElementsByClassName('output')[0].innerText="Division  is "+out
+
+}
