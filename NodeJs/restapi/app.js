cf25d74fecae56b152a86ff6d067713a27eda50f
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2021-10-29 00:33:14 +0530

    mongodb

--- NodeJs/restapi/app.js
+++ NodeJs/restapi/app.js
@@ -40,3 +40,38 @@ app.get('/employee', (req,res)=>{
 app.listen(port, () =>{
     console.log(`Listening on port number ${port}`);
 });
+
+
+
+const mongo = require('mongodb');
+const MongoClient = mongo.MongoClient;
+// const MongoUrl =  "mongodb://localhost:27017";
+const MongoUrl = "mongodb+srv://gaurav:grkml786786@cluster0.gpu6p.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+
+let db ;
+
+    
+//get
+
+app.get('/', (req,res)=>{
+    res.send("Welcome to Node Api.");
+});
+
+
+// Employee
+app.get('/location', (req,res)=>{
+    db.collection('location').find().toArray((err, result )=>{
+        if(err) throw err;
+        res.send(result);
+    });
+});
+
+
+MongoClient.connect(MongoUrl , (err, client)=>{
+    if (err)
+    console.log('Error while connecting.');
+    db = client.db('Zomato');
+    app.listen(port, ()=>{
+        console.log(`listening on port number ${port}`);
+    });
+});
