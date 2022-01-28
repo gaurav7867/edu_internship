520bf1dde6329656f5911acfa11e94536adce323
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-01-28 11:39:11 +0530

    new method used

--- NodeJs/inbuilt_Packages/os.js
+++ NodeJs/inbuilt_Packages/os.js
@@ -4,3 +4,14 @@ console.log(os.arch());
 console.log(os.cpus().length);
 console.log(os.hostname());
 console.log(os.freemem());
+
+const freemem = os.freemem();
+console.log(`${freemem/1024/1024/1024}`);
+
+
+// Shortcut Ctrl + shift + escape
+
+const totalMem = os.totalmem();
+console.log(`${totalMem/1024/1024/1024}`);
+
+console.log(os.type());
