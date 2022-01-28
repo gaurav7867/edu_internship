82c90823f7ee46a8a41a754b7e39cde689064abf
Gaurav Gupta <86770283+gauravgupta799@users.noreply.github.com>
2022-01-28 12:51:56 +0530

    impExp

---
+++ NodeJs/ImportExport/index.js
@@ -0,0 +1,25 @@
+const {add,sub,name,mult} =require('./oper')
+
+console.log(add(10,1))
+console.log(sub(10,1))
+console.log(mult(10,10))
+console.log(name)
+
+
+
+
+
+// const oper =require('./oper')
+// console.log(oper.add(10,1))
+// console.log(oper.sub(10,1))
+// console.log(oper.mult(10,10))
+
+// const add = require('./oper')
+// console.log(add(67,89));
+
+// console.log(add)
+
+// const name = require('./oper')
+// console.log(name)
+
+
---
+++ NodeJs/ImportExport/oper.js
@@ -0,0 +1,23 @@
+const add = (a, b) =>{
+    return a + b;
+}
+
+const sub = (a, b) =>{
+    return a - b;
+}
+
+const mult = (a, b) =>{
+    return a * b;
+}
+
+const name = 'Gaurav Gupta'
+
+
+module.exports = {add,sub,mult,name};
+
+
+// module.exports.add = add;
+// module.exports.sub = sub;
+// module.exports.mult = mult;
+
+// module.exports = add;
